a8fa0d87fe6749770ea4d2e27dd6c55f2d041478
Chapman Shoop <chapman.shoop@gmail.com>
2014-03-23 12:34:11 -0700

    Getting tox to run (no tests, though :p)

--- setup.py
+++ setup.py
@@ -1,40 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    # distutils breaks in Python2 when passing unicode strings in for packages
+    # below. It pains me to have to drop unicode_literals; maybe we'll be able
+    # to figure out how to bring it back soon.
+    #
+    #unicode_literals,
+)
+
 from distutils.core import setup
+from pip.req import parse_requirements
 
-# Check deps
+import nattraverso
 
-class DependencyFailed(Exception): pass
-class VersionCheckFailed(DependencyFailed): pass
 
-PYTHON_VERSION = '2.3'
-TWISTED_VERSION = '2'
+def get_install_requirements():
+    """Returns a requirement list based on the contents of requirements.txt.
+    """
+    return [str(r.req) for r in parse_requirements("requirements.txt")]
 
-# -------------PYTHON VERSION----------
-import sys, os
-if sys.version < PYTHON_VERSION:
-    raise VersionCheckFailed("Python "+PYTHON_VERSION+" or later is required")
-
-#  ------------TWISTED-----------------
-try:
-    import twisted
-except ImportError:
-    raise DependencyFailed("You need Twisted - http://www.twistedmatrix.com/")
-
-from twisted.copyright import version as twisted_version
-if not twisted_version.startswith('SVN') and twisted_version < TWISTED_VERSION:
-    raise VersionCheckFailed("Twisted "+TWISTED_VERSION+" or later is required")
-
-# ---------------SOAPpy-----------------
-try:
-    import SOAPpy
-except ImportError:
-    raise DependencyFailed("You need SOAPpy")
-
-    
-setup(name='nattraverso',
-	version='0.1.1',
-	description='A NAT Traversal library',
-	author='Raphael Slinckx',
-	author_email='raphael@slinckx.net',
-	url='http://raphael.slinckx.net/nattraverso.php',
-	packages=['nattraverso', 'nattraverso.pynupnp']
-	)
+setup(
+    name="nattraverso",
+    version=nattraverso.__version__,
+    description="A NAT Traversal library",
+    license="LGPL",
+    author="Raphael Slinckx, Chapman Shoop",
+    author_email="raphael@slinckx.net, chapman.shoop@gmail.com",
+    url="https://github.com/belovachap/nattraverso",
+    packages=[
+        "nattraverso",
+        "nattraverso.pynupnp",
+    ],
+    data_files=[
+        ("", [
+            "README",
+            "requirements.txt",
+        ]),
+    ],
+    install_requires=get_install_requirements()
+)
